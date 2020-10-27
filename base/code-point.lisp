0d09324306c7031cee2a6d38260f8a425e674ab9
Thomas Bakketun <thomas@bakketun.pro>
2020-10-27 19:41:34 +0100

    scalar-value is a subtype of code-point

---
+++ base/code-point.lisp
@@ -0,0 +1,85 @@
+(deftype code-point-int ()
+  '(integer 0 #x10FFFF))
+
+
+(deftype semi-standard-char ()
+  '(member #\Tab #\Page #\Rubout #\Linefeed #\Return #\Backspace))
+
+
+(defstruct (standard-code-point
+            (:constructor make-standard-code-point (int)))
+  (int nil :type code-point-int :read-only t))
+
+
+(defgeneric generic-code-point-int (code-point)
+  (:documentation
+   "Methods can assume that the arguement is of type code-point")
+  (:method (object)
+    nil))
+
+
+(deftype code-point ()
+  '(or
+    code-point-int
+    standard-code-point
+    standard-char
+    semi-standard-char
+    ;; implementation defined: maybe base-char, character
+    (satisfies generic-code-point-int)))
+
+
+(defun code-point-p (object)
+  (typep object 'code-point))
+
+
+(defparameter +graphic-standard-chars+
+  "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~")
+
+(defparameter +map-code-point-int-to-char+
+  (coerce
+   (loop :for code-point-int :from 0 :to 127
+         :for char := (case code-point-int
+                        (8 #\Backspace)
+                        (9 #\Tab)
+                        (10 #\Linefeed)
+                        (12 #\Page)
+                        (13 #\Return)
+                        (32 #\Space)
+                        (127 #\Rubout)
+                        (otherwise
+                         (when (<= 33 code-point-int 126)
+                           (char +graphic-standard-chars+ (- code-point-int 33)))))
+         :collect char)
+   'vector))
+
+(defparameter +map-char-code-to-code-point-int+
+  (map 'vector (lambda (char)
+                 (and char (char-code char)))
+       +map-code-point-int-to-char+))
+
+
+(defun code-point-int (object)
+  "Return the code point integer designated for the given object, or nil."
+  (typecase object
+    (integer
+     object)
+    (standard-code-point
+     (standard-code-point-int object))
+    ((member #\Newline)
+     10)
+    ((or standard-char semi-standard-char)
+     (svref +map-char-code-to-code-point-int+ (char-code object)))
+    (t
+     (generic-code-point-int object))))
+
+
+(defun standard-code-point (code-point)
+  (etypecase code-point
+    (standard-code-point
+     code-point)
+    (code-point
+     (make-standard-code-point (code-point-int code-point)))))
+
+
+(defun code-point-char (code-point)
+  (svref +map-code-point-int-to-char+ (code-point-int code-point)))
--- base/scalar-value.lisp
+++ base/scalar-value.lisp
@@ -11,85 +11,21 @@
     (integer #xE000 #x10FFFF)))
 
 
-(deftype semi-standard-char ()
-  '(or standard-char
-    (member #\Tab #\Page #\Rubout #\Linefeed #\Return #\Backspace)))
-
-
-(defgeneric generic-scalar-value-p (object)
-  (:documentation
-   "Can be specialized to extend which objects are of type scalar-value.")
-  (:method (object)
-    (typep object `(or scalar-value-int
-                       semi-standard-char))))
-
-
-(defun scalar-value-p (object)
-  "Returns true if object is of type `scalar-value`; otherwise, returns false."
-  (generic-scalar-value-p object))
+(defun scalar-value-int (object)
+  (let ((code-point-int (code-point-int object)))
+    (when (typep code-point-int 'code-point-int)
+      code-point-int)))
 
 
 (deftype scalar-value ()
-  '(satisfies scalar-value-p))
+  '(and code-point (satisfies scalar-value-int)))
 
 
-(defparameter +graphic-standard-chars+
-  "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~")
-
-(defparameter +map-scalar-value-int-to-char+
-  (coerce
-   (loop :for scalar-value-int :from 0 :to 127
-         :for char := (case scalar-value-int
-                        (8 #\Backspace)
-                        (9 #\Tab)
-                        (10 #\Linefeed)
-                        (12 #\Page)
-                        (13 #\Return)
-                        (32 #\Space)
-                        (127 #\Rubout)
-                        (otherwise
-                         (when (<= 33 scalar-value-int 126)
-                           (char +graphic-standard-chars+ (- scalar-value-int 33)))))
-         :collect char)
-   'vector))
-
-(defparameter +map-char-code-to-scalar-value-int+
-  (map 'vector (lambda (char)
-                 (and char (char-code char)))
-       +map-scalar-value-int-to-char+))
-
-(defgeneric generic-scalar-value-int (scalar-value)
-  (:documentation
-   "Methods can assume that the arguement is of type scalar-value")
-  (:method ((int integer))
-    int)
-  (:method ((char character))
-    (case char
-      (#\Newline 10)
-      (otherwise (svref +map-char-code-to-scalar-value-int+ (char-code char))))))
+(defun scalar-value-p (object)
+  (typep object 'scalar-value))
 
 
-(defun scalar-value-int (object)
-  (check-type object scalar-value)
-  (generic-scalar-value-int object))
 
 
-(defun scalar-value-standard-char (scalar-value)
-  "Return the semi standard character object denoting the given
-scalar-value or NIL if no such character exists."
-  (svref +map-scalar-value-int-to-char+ (scalar-value-int scalar-value)))
 
 
-(defgeneric generic-scalar-value-char (scalar-value)
-  (:method (object)
-    nil))
-
-
-(defun scalar-value-char (scalar-value)
-  "Return the character object denoting the given scalar-value or NIL
-if no such character exists.
-
-Which scalar values that can be represented by a character is
-implementation defined, except for the standard and semi standard characters."
-  (or (scalar-value-standard-char scalar-value)
-      (generic-scalar-value-char scalar-value)))
