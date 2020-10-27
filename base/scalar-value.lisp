0bfc9313ec117e1d2184012a4d08765ff507fabf
Thomas Bakketun <thomas@bakketun.pro>
2020-10-27 19:53:22 +0100

    Remove scalar-value-int type.

--- base/scalar-value.lisp
+++ base/scalar-value.lisp
@@ -1,24 +1,11 @@
-;;; Unicode text is a sequence of scalar values.
-;;;
-;;; Scalar values are allways represented by designator objects. There is no native scalar value class.
-;;;
-;;; Using characters as scalar values is well defined only for standard and semi standard characters.
-
-(deftype scalar-value-int ()
-  "An integer represenation of a scalar value."
-  '(or
-    (integer 0 #xD7FF)
-    (integer #xE000 #x10FFFF)))
-
-
-(defun scalar-value-int (object)
+(defun scalar-value (object)
   (let ((code-point-int (code-point-int object)))
-    (when (typep code-point-int 'code-point-int)
+    (unless (typep code-point-int '(integer #xD800 #xDFFF))
       code-point-int)))
 
 
 (deftype scalar-value ()
-  '(and code-point (satisfies scalar-value-int)))
+  '(and code-point (satisfies scalar-value)))
 
 
 (defun scalar-value-p (object)
