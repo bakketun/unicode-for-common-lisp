94755e5ab5a73b41ef84cb587eae1e37c732b765
Thomas Bakketun <thomas@bakketun.pro>
2020-10-28 17:58:13 +0100

    Fixed implemetion defined type for code point.

--- base/code-point.lisp
+++ base/code-point.lisp
@@ -1,5 +1,99 @@
-(deftype code-point-int ()
+
+;#-(or code-point-is-integer code-point-is-character code-point-is-class)
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  ;(pushnew :code-point-is-integer *features*)
+  ;(pushnew :code-point-is-character *features*)
+  (pushnew :code-point-is-class *features*))
+
+
+(deftype code-point-code ()
+  "The subtype of integer that can represent every code point."
   '(integer 0 #x10FFFF))
+
+
+(defstruct (standard-code-point
+            (:constructor make-standard-code-point (code)))
+  "A class only used to represent code points."
+  (code nil :type code-point-code :read-only t))
+
+
+(defgeneric generic-code-point (object)
+  (:documentation
+   "Allows the function code-point to be extended.")
+  (:method (object)
+    nil))
+
+
+(deftype code-point ()
+  #+code-point-is-class
+  'standard-code-point
+  #+code-point-is-integer
+  'code-point-code
+  #+code-point-is-character
+  'character)
+
+
+(defun code-point (object)
+  (typecase object
+    (code-point
+     object)
+    (code-point-code
+     #+code-point-is-class
+     (make-standard-code-point object)
+     #+code-point-is-integer
+     object
+     #+code-point-is-character
+     (code-char object))
+    (t
+     (generic-code-point object))))
+
+
+(defun code-point-code (object)
+  (typecase object
+    (null
+     nil)
+    (code-point-code
+     object)
+    (standard-code-point
+     (standard-code-point-code object))
+    #+code-point-is-character
+    (character
+     (char-code object))
+    (t
+     (code-point-code (code-point object)))))
+
+
+(deftype surrogate-code-point-code ()
+  '(integer #xD800 #xDFFF))
+
+
+(defun surrogate-code-point-p (object)
+  (when (typep (code-point-code object) 'surrogate-code-point-code)
+    object))
+
+
+(deftype surrogate-code-point ()
+  '(and code-point
+    (satisfies surrogate-code-point-p)))
+
+
+(deftype scalar-value ()
+  '(and code-point-code
+    (not surrogate-code-point-code)))
+
+
+(defun scalar-value-p (object)
+  (let ((code (code-point-code object)))
+    (when (typep code 'scalar-value)
+      code)))
+
+
+(defun scalar-value (object)
+  (let ((code-point-code (code-point-code object)))
+    (check-type code-point-code scalar-value)
+    code-point-code))
+
+
 
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
@@ -20,102 +114,37 @@
   `(member ,@(mapcar #'car +semi-standard-char-name-map+)))
 
 
-(defstruct (standard-code-point
-            (:constructor make-standard-code-point (int)))
-  (int nil :type code-point-int :read-only t))
-
-
-(defgeneric generic-code-point-int (code-point)
-  (:documentation
-   "Methods can assume that the arguement is of type code-point")
-  (:method (object)
-    nil))
-
-
-(deftype code-point ()
-  '(or
-    code-point-int
-    standard-code-point
-    (and standard-char
-     (not (member #\Newline)))
-    semi-standard-char
-    (satisfies generic-code-point-int)))
-
-
 (defparameter +graphic-and-space-standard-chars+
   " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~")
 
 
-(defparameter +map-char-to-code-point-int+
+(defparameter +map-char-to-code-point+
   (make-hash-table))
 
 
-(defparameter +map-code-point-int-to-char+
+(defparameter +map-code-point-code-to-char+
   (coerce
-   (loop :for code-point-int :from 0 :to 127
-         :for char := (typecase code-point-int
+   (loop :for code-point-code :from 0 :to 127
+         :for char := (typecase code-point-code
                         ((integer 32 126)
-                         (char +graphic-and-space-standard-chars+ (- code-point-int 32)))
+                         (char +graphic-and-space-standard-chars+ (- code-point-code 32)))
                         (t
-                         (car (find code-point-int +semi-standard-char-name-map+ :key #'cdr))))
+                         (car (find code-point-code +semi-standard-char-name-map+ :key #'cdr))))
          :when char
-           :do (setf (gethash char +map-char-to-code-point-int+) code-point-int)
+           :do (setf (gethash char +map-char-to-code-point+) (code-point code-point-code))
          :collect char)
    'vector))
 
 
-(defun code-point-p (object)
-  "If object type code-point returns the integer value of the code point, othewise NIL.
-
-Note: #\Newline might or might not map to a code-point."
-  (typecase object
-    (code-point-int
-     object)
-    (standard-code-point
-     (standard-code-point-int object))
-    ((or standard-char semi-standard-char)
-     (values (gethash object +map-char-to-code-point-int+)))
-    (t
-     (generic-code-point-int object))))
-
-
-(defun code-point-code (object)
-  (check-type object code-point)
-  (code-point-p object))
-
-
-(defun standard-code-point (code-point)
-  (etypecase code-point
-    (standard-code-point
-     code-point)
-    (code-point
-     (make-standard-code-point (code-point-code code-point)))))
+(defun char-code-point (char)
+  (values (gethash char +map-char-to-code-point+)))
 
 
 (defun code-point-char (code-point)
-  (svref +map-code-point-int-to-char+ (code-point-code code-point)))
+  (let ((code (code-point-code code-point)))
+    (when code
+      (svref +map-code-point-code-to-char+ code))))
 
 
-(defun surrogate-code-point-p (object)
-  (when (typep (code-point-code object) '(integer #xD800 #xDFFF))
-    object))
-
-
-(deftype surrogate-code-point ()
-  '(and code-point
-    (satisfies surrogate-code-point-p)))
-
-
-(deftype scalar-value ()
-  '(and code-point
-    (not surrogate-code-point)))
-
-
-(defun scalar-value (object)
-  (check-type object scalar-value)
-  (scalar-value-p object))
-
-
-(defun scalar-value-p (object)
-  (when (typep object 'scalar-value)
-    (code-point-code object)))
+(defmethod generic-code-point ((char character))
+  (char-code-point char))
--- base/tests.lisp
+++ base/tests.lisp
@@ -1,4 +1,3 @@
 (assert (subtypep 'surrogate-code-point 'code-point))
-(assert (subtypep 'scalar-value 'code-point))
-(when (eq #\Newline (name-char "Linefeed"))
-  (assert (subtypep 'standard-char 'code-point)))
+(assert (subtypep 'scalar-value 'code-point-code))
+(assert (not (code-point #\Newline)))
