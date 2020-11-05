64db3e81866645454363a77fe591cad3dd38cc5d
Thomas Bakketun <thomas@bakketun.pro>
2020-11-05 20:21:46 +0100

    Use nobreak space of alignment.

--- convert/utf-8-encoder.lisp
+++ convert/utf-8-encoder.lisp
@@ -1,18 +1,18 @@
 (defun code-point-utf-8-encode (code-point)
-  (etypecase code-point ;         byte count     upper bits             bit size  bit position
-    ;;            from   to                |              |                    |  |
-    ((integer     #x00 #x7f)       (values 1                                          code-point))
-    ((integer   #x0080 #x07ff)     (values 2    (logior #b11000000  (ldb (byte 5  6)  code-point))
-                                           #||# (logior #b10000000  (ldb (byte 6  0)  code-point))))
+  (etypecase code-point ;         byte count    upper bits             bit size  bit position
+    ;;            from   to                |             |                    |  |
+    ((integer     #x00 #x7f)       (values 1                                         code-point))
+    ((integer   #x0080 #x07ff)     (values 2   (logior #b11000000  (ldb (byte 5  6)  code-point))
+                                               (logior #b10000000  (ldb (byte 6  0)  code-point))))
     ((integer   #x0800 #xffff)     (values 3
-                                           #||# (logior #b11100000  (ldb (byte 4 12)  code-point))
-                                           #||# (logior #b10000000  (ldb (byte 6  6)  code-point))
-                                           #||# (logior #b10000000  (ldb (byte 6  0)  code-point))))
+                                               (logior #b11100000  (ldb (byte 4 12)  code-point))
+                                               (logior #b10000000  (ldb (byte 6  6)  code-point))
+                                               (logior #b10000000  (ldb (byte 6  0)  code-point))))
     ((integer #x100000 #x10ffff)   (values 4
-                                           #||# (logior #b11110000  (ldb (byte 3 18)  code-point))
-                                           #||# (logior #b10000000  (ldb (byte 6 12)  code-point))
-                                           #||# (logior #b10000000  (ldb (byte 6  6)  code-point))
-                                           #||# (logior #b10000000  (ldb (byte 6  0)  code-point))))))
+                                               (logior #b11110000  (ldb (byte 3 18)  code-point))
+                                               (logior #b10000000  (ldb (byte 6 12)  code-point))
+                                               (logior #b10000000  (ldb (byte 6  6)  code-point))
+                                               (logior #b10000000  (ldb (byte 6  0)  code-point))))))
 
 
 (defun utf-8-encode-into (code-points vector &key start end)
@@ -21,7 +21,7 @@
         :for code-point :in code-points
         :do (multiple-value-bind (size b0 b1 b2 b3) (code-point-utf-8-encode code-point)
               (assert (<= (+ pos size) end)  () "Not enough room.")
-              #| first byte |#   (setf (aref vector pos) b0)   (incf pos)
+                                 (setf (aref vector pos) b0)   (incf pos)
               (when (< 1 size)   (setf (aref vector pos) b1)   (incf pos))
               (when (< 2 size)   (setf (aref vector pos) b2)   (incf pos))
               (when (< 3 size)   (setf (aref vector pos) b3)   (incf pos)))
