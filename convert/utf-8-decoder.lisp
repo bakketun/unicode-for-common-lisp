5da537a438ae5133154d9f0838d05020728d2967
Thomas Bakketun <thomas@bakketun.pro>
2020-11-06 13:47:31 +0100

    More layout

--- convert/utf-8-decoder.lisp
+++ convert/utf-8-decoder.lisp
@@ -71,23 +71,23 @@
   ;; 3
   (when (zerop bytes-needed)
     (typecase %byte
-      ;;  Inclusive range    condition for action      Action                value to set
-      ((integer #x00 #x7f)   ~    ~    ~    ~    ~    (%return-code-point %byte))
+      ;;  inclusive range  | condition              | action | place          | value
+      ((integer #x00 #x7f)   ~    ~    ~    ~    ~    (%return-code-point       %byte))
 
-      ((integer #xc2 #xdf)   ~    ~    ~    ~    ~    (setf bytes-needed     1
-                                                            code-point       (ldb (byte 5 0) %byte)))
+      ((integer #xc2 #xdf)   ~    ~    ~    ~    ~    (setf    bytes-needed     1
+                                                               code-point       (ldb (byte 5 0) %byte)))
 
-      ((integer #xe0 #xef)   (when (eql %byte #xe0)   (setf lower-boundary   #xa0))
-       ~                     (when (eql %byte #xed)   (setf upper-boundary   #x9f))
-       ~                     ~    ~    ~    ~    ~    (setf bytes-needed     2
-                                                            code-point       (ldb (byte 4 0) %byte)))
+      ((integer #xe0 #xef)   (when (eql %byte #xe0)   (setf    lower-boundary   #xa0))
+       ~                     (when (eql %byte #xed)   (setf    upper-boundary   #x9f))
+       ~                     ~    ~    ~    ~    ~    (setf    bytes-needed     2
+                                                               code-point       (ldb (byte 4 0) %byte)))
 
-      ((integer #xF0 #xF4)   (when (eql %byte #xf0)   (setf lower-boundary   #x90))
-       ~                     (when (eql %byte #xf4)   (setf upper-boundary   #x8f))
-       ~                     ~    ~    ~    ~    ~    (setf bytes-needed     3
-                                                            code-point       (ldb (byte 3 0) %byte)))
+      ((integer #xF0 #xF4)   (when (eql %byte #xf0)   (setf    lower-boundary   #x90))
+       ~                     (when (eql %byte #xf4)   (setf    upper-boundary   #x8f))
+       ~                     ~    ~    ~    ~    ~    (setf    bytes-needed     3
+                                                               code-point       (ldb (byte 3 0) %byte)))
 
-      (t #|  Otherwise  |#   ~    ~    ~    ~    ~    (%return-error :invalid-first-byte)))
+      (t #|  Otherwise  |#   ~    ~    ~    ~    ~    (%return-error            :invalid-first-byte)))
     (%return-continue))
   ;; 4
   (unless (<= lower-boundary %byte upper-boundary)
