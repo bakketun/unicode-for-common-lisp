8082d5e489159382d6d1cdda2ce61e172f369a8b
Thomas Bakketun <thomas@bakketun.pro>
2020-11-12 20:49:27 +0100

    Fix feature detection.

--- common/features.lisp
+++ common/features.lisp
@@ -1,25 +1,33 @@
 (in-package #:unicode-common)
 
 
-#-(or string-is-utf-8        ;; char-code = UTF-8 code unit
-      string-is-utf-16       ;; char-code = UTF-16 code unit
-      string-is-utf-32       ;; char-code = UTF-32 code unit
-      string-is-not-unicode  ;; char-code = something else, e.g. EBCDIC
-      )
-(pushnew (cond ((<= +code-point-limit+ char-code-limit)
-                :string-is-utf-32)
-               ((= +bmp-code-point-limit+ char-code-limit)
-                :string-is-utf-16)
-               ((<= #xFF char-code-limit)
-                :string-is-utf-8)
-               (t
-                :string-is-not-unicode))
-         *features*)
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun features-detect (cclimit)
+    (pushnew (cond ((<= +code-point-limit+ cclimit)
+                    :string-is-utf-32)
+                   ((= +bmp-code-point-limit+ cclimit)
+                    :string-is-utf-16)
+                   ((<= #xFF cclimit)
+                    :string-is-utf-8)
+                   (t
+                    :string-is-not-unicode))
+             *features*))
 
-(let ((enabled-features (intersection '(:string-is-utf-8
-                                        :string-is-utf-16
-                                        :string-is-utf-32
-                                        :string-is-not-unicode)
-                                      *features*)))
-  (assert (= 1 (length enabled-features)) ()
-          "More than one Unicode feature in *features* ~S.~&Try a clean recompile." enabled-features))
+  (defun enabled-features ()
+    (intersection '(:string-is-utf-8
+                    :string-is-utf-16
+                    :string-is-utf-32
+                    :string-is-not-unicode)
+                  *features*))
+
+  (unless (enabled-features)
+    (features-detect char-code-limit))
+
+  (assert (= 1 (length (enabled-features))) ()
+          "More than one Unicode feature in *features*: ~S." (enabled-features)))
+
+
+(let ((at-read-time #.(car (enabled-features)))
+      (at-run-time (car (enabled-features))))
+  (assert (eql at-read-time at-run-time) ()
+          "Unicode feature changed from ~S to ~S. Please do clean recompile." at-read-time at-run-time))
